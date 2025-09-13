01951a51e906ed9441d3dc28127ab4adcb1d4657
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2025-09-13 11:59:50 +0200

    Base app lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,5 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   formatters: true,
-  // rules: {
-  //   'react/react-in-jsx-scope': 'off', // React 17+ no requiere importar React
-  // },
+  react: true,
 })
